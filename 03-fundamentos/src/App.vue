e801da727244ac3af58b4d1081fd04ceb79742b8
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-08-29 19:07:47 -0600

    IndecisionApp: Primera app

--- 03-fundamentos/src/App.vue
+++ 03-fundamentos/src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
   
-  <counter :start="101"/>
+  <Indecision />
+
+  <!-- <counter :start="101"/> -->
 
 </template>
 
 <script>
-import Counter from '@/components/Counter.vue'
+// import Counter from '@/components/Counter.vue'
+import Indecision from '@/components/Indecision.vue'
 
 export default {
   name: 'App',
   components: {
-    Counter
+    // Counter,
+    Indecision
   }
 }
 </script>
