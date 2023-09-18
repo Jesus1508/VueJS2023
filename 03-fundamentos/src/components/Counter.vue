7b5a948a0e3bae027a80168cc4d38ba0a61f6434
Jesus Solis Martinez <iscjesus1508@gmail.com>
2023-09-18 08:32:23 -0600

    Coontinuando pruebas unitarias

--- 03-fundamentos/src/components/Counter.vue
+++ 03-fundamentos/src/components/Counter.vue
@@ -2,7 +2,7 @@ import Counter from '@/components/Counter.vue';
 <template lang="">
     <h2>{{ customTitle }}!!!</h2>
     <p>{{ counter }} <sup>2</sup> = {{ squareCounter}}</p>
-    
+    <p data-testid='counter'>{{ counter }}</p>
     <div>
         <button @click="increase">+1</button>
         <button @click="decrease">-1</button>
--- 03-fundamentos/tests/unit/components/counter.spec.js
+++ 03-fundamentos/tests/unit/components/counter.spec.js
@@ -2,11 +2,34 @@ import Counter from "@/components/Counter";
 import { shallowMount, mount } from "@vue/test-utils";
 
 describe('Counter Component', () => {
-    test('debe de hacer match con el snapshot', () => {
+    // test('debe de hacer match con el snapshot', () => {
+        
+    //     const wrapper = shallowMount(Counter)
+        
+    //     expect( wrapper.html() ).toMatchSnapshot()
+
+    // });
+
+    test('h2 debe de tener el valor por defecto "Counter"', () => {
         
         const wrapper = shallowMount(Counter)
+
+        expect (wrapper.find('h2').exists()).toBeTruthy()
+
+        const h2value = wrapper.find('h2').text()
+
+        expect(h2value).toBe('Counter!!!')
+    });
+
+    test('el valor por defecto debe de ser 10 en el p', () => {
         
-        expect( wrapper.html() ).toMatchSnapshot()
+        const wrapper = shallowMount(Counter)
+
+        // const pTags = wrapper.findAll('p')
+        const value =wrapper.find('[data-testid="counter"]').text()
+
+        // expect( pTags[1].text() ).toBe('10')
+        expect(value).toBe('10')
 
     });
 })
